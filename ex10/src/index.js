eda2d3197cea478bfbb8314014ec5fbb161d4ecf
selmakahvedzic <selma.kahvedzic97@gmail.com>
2021-03-11 13:37:01 +0100

    ex10 done ! String

---
+++ ex10/src/index.js
@@ -0,0 +1,15 @@
+function main(){
+
+    var myFirstName = "Selma";
+    var myLastName = "Kahvedzic";
+    var myHomeTown ="Sarajevo";
+
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+  }
+  
+  console.log(main());
+  module.exports = main;
